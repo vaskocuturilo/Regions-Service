e60bdb1b104381a85d50b6a637f4e0c99994e42c
Anton <Smirnov>
2021-08-04 20:31:10 +0300

    DV-0002: Add functional for search region number.

---
+++ src/main/java/com/regions/simpleRegions/exception/RegionAlreadyExistException.java
@@ -0,0 +1,7 @@
+package com.regions.simpleRegions.exception;
+
+public class RegionAlreadyExistException extends Exception {
+    public RegionAlreadyExistException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/regions/simpleRegions/service/RussiaService.java
@@ -0,0 +1,35 @@
+package com.regions.simpleRegions.service;
+
+import com.regions.simpleRegions.entity.RussiaEntity;
+import com.regions.simpleRegions.exception.RegionAlreadyExistException;
+import com.regions.simpleRegions.exception.RegionNotFoundException;
+import com.regions.simpleRegions.model.RussiaModel;
+import com.regions.simpleRegions.respository.RussiaRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RussiaService {
+
+    @Autowired
+    RussiaRepo russiaRepo;
+
+    public RussiaEntity createRegion(RussiaEntity russiaEntity) throws RegionAlreadyExistException {
+        if (russiaRepo.findByRegion(russiaEntity.getRegion()) != null) {
+            throw new RegionAlreadyExistException("Region is exist.");
+        }
+        return russiaRepo.save(russiaEntity);
+    }
+
+    public RussiaModel getOne(String region) throws RegionNotFoundException {
+        RussiaEntity russiaRegion = russiaRepo.findByRegion(region);
+        if (russiaRegion == null) {
+            throw new RegionNotFoundException("Region not found.");
+        }
+        return RussiaModel.toModel(russiaRegion);
+    }
+
+    public Iterable<RussiaEntity> getAllRegions() {
+        return russiaRepo.findAll();
+    }
+}
