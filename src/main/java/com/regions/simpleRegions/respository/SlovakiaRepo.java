212ce58a7efeb0affef0775a4d46d7b39ab61abc
vaskocuturilo <vaskocuturilo@rambler.ru>
2021-08-26 21:50:07 +0300

    DV-00014: Add Slovakia functional.

---
+++ src/main/java/com/regions/simpleRegions/respository/SlovakiaRepo.java
@@ -0,0 +1,9 @@
+package com.regions.simpleRegions.respository;
+
+import com.regions.simpleRegions.entity.SlovakiaEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SlovakiaRepo extends CrudRepository<SlovakiaEntity, Long> {
+
+    SlovakiaEntity findByRegion(String region);
+}
---
+++ src/main/java/com/regions/simpleRegions/service/SlovakiaService.java
@@ -0,0 +1,23 @@
+package com.regions.simpleRegions.service;
+
+import com.regions.simpleRegions.entity.SlovakiaEntity;
+import com.regions.simpleRegions.exception.RegionNotFoundException;
+import com.regions.simpleRegions.model.SlovakiaModel;
+import com.regions.simpleRegions.respository.SlovakiaRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SlovakiaService {
+
+    @Autowired
+    SlovakiaRepo slovakiaRepo;
+
+    public SlovakiaModel getOne(String region) throws RegionNotFoundException {
+        SlovakiaEntity slovakiaRegion = slovakiaRepo.findByRegion(region);
+        if (slovakiaRegion == null) {
+            throw new RegionNotFoundException("Region not found.");
+        }
+        return SlovakiaModel.toModel(slovakiaRegion);
+    }
+}
