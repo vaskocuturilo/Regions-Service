41dd7ee011150be8904a4c36cc92cb263910578b
vaskocuturilo <vaskocuturilo@rambler.ru>
2022-01-20 20:18:25 +0300

    DV-00041: Add Azerbaijan country.

---
+++ src/main/java/com/regions/simpleRegions/service/AzerbaijanService.java
@@ -0,0 +1,23 @@
+package com.regions.simpleRegions.service;
+
+import com.regions.simpleRegions.entity.AzerbaijanEntity;
+import com.regions.simpleRegions.exception.RegionNotFoundException;
+import com.regions.simpleRegions.model.AzerbaijanModel;
+import com.regions.simpleRegions.respository.AzerbaijanRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AzerbaijanService {
+
+    @Autowired
+    AzerbaijanRepo azerbaijanRepo;
+
+    public AzerbaijanModel getOne(String region) throws RegionNotFoundException {
+        AzerbaijanEntity azerbaijanRegion = azerbaijanRepo.findByRegion(region);
+        if (azerbaijanRegion == null) {
+            throw new RegionNotFoundException("Region not found.");
+        }
+        return AzerbaijanModel.toModel(azerbaijanRegion);
+    }
+}
