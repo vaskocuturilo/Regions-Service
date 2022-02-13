46353c716fbf361309b4e045301580f9d3eb293d
vaskocuturilo <vaskocuturilo@rambler.ru>
2022-02-13 15:52:40 +0300

    DV-00068: Add a new endpoint for Croatia.

--- src/main/java/com/regions/simpleRegions/service/CroatiaService.java
+++ src/main/java/com/regions/simpleRegions/service/CroatiaService.java
@@ -3,23 +3,29 @@ package com.regions.simpleRegions.service;
 import com.regions.simpleRegions.entity.CroatiaEntity;
 import com.regions.simpleRegions.exception.RegionNotFoundException;
 import com.regions.simpleRegions.model.CroatiaModel;
-import com.regions.simpleRegions.respository.CroatiaRepo;
+import com.regions.simpleRegions.respository.CroatiaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CroatiaService {
 
-    CroatiaRepo croatiaRepo;
+    CroatiaRepository croatiaRepository;
 
-    public CroatiaService(CroatiaRepo croatiaRepo) {
-        this.croatiaRepo = croatiaRepo;
+    @Autowired
+    public CroatiaService(CroatiaRepository croatiaRepository) {
+        this.croatiaRepository = croatiaRepository;
     }
 
     public CroatiaModel getOne(String region) throws RegionNotFoundException {
-        CroatiaEntity croatiaRegion = croatiaRepo.findByRegion(region);
+        CroatiaEntity croatiaRegion = croatiaRepository.findByRegion(region);
         if (croatiaRegion == null) {
             throw new RegionNotFoundException("Region not found.");
         }
         return CroatiaModel.toModel(croatiaRegion);
+    }
+
+    public Iterable<CroatiaEntity> getAllRegions() {
+        return croatiaRepository.findAll();
     }
 }
