58806a30851151abbde3ade0d200541acf83043b
asm <vaskocuturilo@rambler.ru>
2023-05-18 19:15:07 +0200

    DV-000109: Add british age indetifiers plates

---
+++ src/main/java/com/regions/simpleRegions/exception/DateRegistrationException.java
@@ -0,0 +1,8 @@
+package com.regions.simpleRegions.exception;
+
+public class DateRegistrationException extends Exception {
+
+    public DateRegistrationException(String message) {
+        super(message);
+    }
+}
