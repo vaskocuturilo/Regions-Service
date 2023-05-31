badb3b5a34a02a08c92d5e363193fee21602c742
asm <vaskocuturilo@rambler.ru>
2023-05-31 22:20:56 +0200

    Fix: fixed drop-down functionality.

--- src/main/resources/static/js/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('#dropdown').on('change', function () {
-        if (['AM', 'AU', 'AZ', 'BY', 'GB', 'BG', 'HR', 'CZ', 'EE', 'FR', 'DE', 'GR', 'HU', 'IE', 'IT', 'KZ', 'KO', 'KG', 'LT', 'MD', 'ME', 'NO', 'PL', 'RO', 'RU', 'SK', 'SI', 'SE', 'CH', 'TR', 'UA', 'UZ'].indexOf(this.value) > -1) {
-            $("#image").classList.remove('hidden');
-        } else {
-            $("#image").classList.add('hidden');
-        }
-    }).change();
-});
