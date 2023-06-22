60f78374b60c77bcdd196f7a4979a14c47d51a31
asm <vaskocuturilo@rambler.ru>
2023-06-22 19:52:02 +0200

    DV-000150: Fix Croatia country.

--- src/main/java/com/regions/simpleRegions/controller/CroatiaController.java
+++ src/main/java/com/regions/simpleRegions/controller/CroatiaController.java
@@ -1,36 +1,39 @@
 package com.regions.simpleRegions.controller;
 
-import com.regions.simpleRegions.exception.RegionNotFoundException;
 import com.regions.simpleRegions.exception.RegionsNotFoundException;
 import com.regions.simpleRegions.service.CroatiaService;
+import lombok.Data;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Data
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1/croatia")
 public class CroatiaController {
+    private final CroatiaService croatiaService;
 
-    CroatiaService croatiaService;
-
-    public CroatiaController(CroatiaService croatiaService) {
-        this.croatiaService = croatiaService;
-    }
-
-    @GetMapping("/croatia")
-    public ResponseEntity getCroatiaRegionByNumber(@RequestParam String region) {
+    @GetMapping("/region/{region}")
+    public ResponseEntity getCroatiaRegionByNumber(@PathVariable String region) {
         try {
-            return ResponseEntity.ok(croatiaService.getOne(region));
-        } catch (RegionNotFoundException exception) {
-            return ResponseEntity.badRequest().body(exception.getMessage());
+            return ResponseEntity.ok(croatiaService.getRegionByName(region));
         } catch (Exception exception) {
             return ResponseEntity.badRequest().body(exception.getMessage());
         }
     }
 
-    @GetMapping("/croatia/all")
+    @GetMapping("/description/{description}")
+    public ResponseEntity getCroatiaRegionByDescription(@PathVariable String description) {
+        try {
+            return ResponseEntity.ok(croatiaService.getRegionByDescription(description));
+        } catch (Exception exception) {
+            return ResponseEntity.badRequest().body(exception.getMessage());
+        }
+    }
+
+    @GetMapping("/all")
     public ResponseEntity getAllCroatiaRegions() {
         try {
             return ResponseEntity.ok(croatiaService.getAllRegions());
--- src/main/java/com/regions/simpleRegions/service/CroatiaService.java
+++ src/main/java/com/regions/simpleRegions/service/CroatiaService.java
@@ -1,27 +1,39 @@
 package com.regions.simpleRegions.service;
 
 import com.regions.simpleRegions.entity.CroatiaEntity;
-import com.regions.simpleRegions.exception.RegionNotFoundException;
+import com.regions.simpleRegions.exception.RegionsNotFoundException;
 import com.regions.simpleRegions.model.CroatiaModel;
 import com.regions.simpleRegions.respository.CroatiaRepo;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.Data;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Data
 @Service
 public class CroatiaService {
-    CroatiaRepo croatiaRepo;
+    private final CroatiaRepo croatiaRepo;
 
-    @Autowired
-    public CroatiaService(CroatiaRepo croatiaRepo) {
-        this.croatiaRepo = croatiaRepo;
+    public CroatiaModel getRegionByName(String region) {
+        Optional<CroatiaEntity> croatiaRegion = croatiaRepo.findByRegion(region);
+        croatiaRegion.stream().filter(croatiaEntity -> croatiaEntity.getRegion().equals(region)).findFirst().orElseThrow(
+                () -> {
+                    RegionsNotFoundException regionsNotFoundException = new RegionsNotFoundException(String.format("Region with %s not found in database.", region));
+                    return regionsNotFoundException;
+                });
+
+        return CroatiaModel.toModelRegion(croatiaRegion);
     }
 
-    public CroatiaModel getOne(String region) throws RegionNotFoundException {
-        CroatiaEntity croatiaRegion = croatiaRepo.findByRegion(region);
-        if (croatiaRegion == null) {
-            throw new RegionNotFoundException("Region not found.");
-        }
-        return CroatiaModel.toModel(croatiaRegion);
+    public List<CroatiaModel> getRegionByDescription(String description) throws RegionsNotFoundException {
+        List<CroatiaEntity> croatiaEntities = croatiaRepo.findByDescription(description);
+
+        if (croatiaEntities.isEmpty())
+            throw new RegionsNotFoundException("Description with " + description + " not found in database.");
+
+        return croatiaEntities.stream().map(CroatiaModel::toModelDescription).collect(Collectors.toList());
     }
 
     public Iterable<CroatiaEntity> getAllRegions() {
