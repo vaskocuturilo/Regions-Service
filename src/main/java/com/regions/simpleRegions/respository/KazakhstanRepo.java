aa077c8d7eeadcfcac8da06bb88685487c115262
asm <vaskocuturilo@rambler.ru>
2023-06-27 18:33:42 +0200

    DV-000157: Fix Kazakhstan country.

--- src/main/java/com/regions/simpleRegions/respository/KazakhstanRepo.java
+++ src/main/java/com/regions/simpleRegions/respository/KazakhstanRepo.java
@@ -1,11 +1,19 @@
 package com.regions.simpleRegions.respository;
 
 import com.regions.simpleRegions.entity.KazakhstanEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface KazakhstanRepo extends CrudRepository<KazakhstanEntity, Long> {
 
-    KazakhstanEntity findByRegion(String region);
+    Optional<KazakhstanEntity> findByRegion(@PathVariable String region);
+
+    @Query("select kazakhstan from KazakhstanEntity kazakhstan WHERE kazakhstan.description LIKE CONCAT('%',:description,'%')")
+    List<KazakhstanEntity> findByDescription(@PathVariable String description);
 }
