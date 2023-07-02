61f6e5c8ba96ea550e70b01b1e971761f1ed04a4
asm <vaskocuturilo@rambler.ru>
2023-07-02 18:10:08 +0200

    DV-000166: Fix Romania country.

--- src/test/java/com/regions/simpleregions/countries/SimpleIntegrationRomaniaTest.java
+++ src/test/java/com/regions/simpleregions/countries/SimpleIntegrationRomaniaTest.java
@@ -2,6 +2,7 @@ package com.regions.simpleregions.countries;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -20,22 +21,55 @@ class SimpleIntegrationRomaniaTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Value("${notification.description.message}")
+    private String descriptionNotFound;
+
+    @Value("${notification.region.message}")
+    private String regionNotFound;
+
+    private String PATH = "/api/v1/romania/plates";
+
     @Test
     void getRegionHandle_whenGetRomania_thenStatus200() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders
-                        .get("/api/romania")
-                        .param("region", "AG")
+                        .get(PATH + "/region/AG")
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("$.region").isNotEmpty())
+                .andExpect(jsonPath("$.region", equalTo("AG")))
                 .andExpect(jsonPath("$.description").isNotEmpty())
                 .andExpect(jsonPath("$.description", equalTo("Argeș")));
     }
 
     @Test
+    void getRegionHandle_whenGetRomaniaPlatesByDescription_thenStatus200() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get(PATH + "/description/Argeș")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[*]").isNotEmpty())
+                .andExpect(jsonPath("$[*].region").isNotEmpty())
+                .andExpect(jsonPath("$[*].description").isNotEmpty())
+                .andExpect(jsonPath("$[*]", hasSize(1)));
+    }
+
+    @Test
+    void getRegionHandle_whenGetRomaniaPlatesByDescriptionContains_thenStatus200() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get(PATH + "/description/Ar")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].region", equalTo("AG")))
+                .andExpect(jsonPath("$[0].description", equalTo("Argeș")));
+    }
+
+    @Test
     void getRegionHandle_whenGetAllRomaniaRegions_thenStatus200() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders
-                        .get("/api/romania/all")
+                        .get(PATH + "/all")
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
@@ -44,14 +78,43 @@ class SimpleIntegrationRomaniaTest {
     }
 
     @Test
-    void getRegionHandle_whenExceptionRomania_thenStatus200() throws Exception {
+    void getRegionHandle_whenGetRomaniaPlatesWithoutDescription_thenStatus404() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders
-                        .get("/api/romania")
-                        .param("region", "AGA")
+                        .get(PATH + "/description/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void getRegionHandle_whenGetRomaniaPlatesWithoutRegion_thenStatus404() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get(PATH + "/region/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void getRegionHandle_whenExceptionRomaniaPlatesByRegion_thenStatus400() throws Exception {
+        String region = "60";
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get(PATH + "/region/" + region)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest()).
+                andExpect(MockMvcResultMatchers.content().string(String.format(regionNotFound, region)));
+
+    }
+
+    @Test
+    void getRegionHandle_whenExceptionRomaniaPlatesByDescription_thenStatus400() throws Exception {
+        String description = "WWWWWWW";
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get(PATH + "/description/" + description)
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest())
-                .andExpect(MockMvcResultMatchers.content().string("Region not found."));
+                .andExpect(MockMvcResultMatchers.content().string(String.format(descriptionNotFound, description)));
     }
-
 }
