9c14918141bb57030275c35de3c26b60f73628f3
asm <vaskocuturilo@rambler.ru>
2023-07-02 20:48:46 +0200

    DV-000171: Fix Uzbekistan country.

--- src/main/java/com/regions/simpleregions/controller/UzbekistanController.java
+++ src/main/java/com/regions/simpleregions/controller/UzbekistanController.java
@@ -1,44 +1,44 @@
 package com.regions.simpleregions.controller;
 
 
+import com.regions.simpleregions.entity.UzbekistanEntity;
+import com.regions.simpleregions.exception.DescriptionNotFoundException;
 import com.regions.simpleregions.exception.RegionNotFoundException;
-import com.regions.simpleregions.exception.RegionsNotFoundException;
 import com.regions.simpleregions.service.UzbekistanService;
+import lombok.Data;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Data
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1/uzbekistan/plates")
 public class UzbekistanController {
 
-    UzbekistanService uzbekistanService;
+    private final UzbekistanService uzbekistanService;
 
-    public UzbekistanController(UzbekistanService uzbekistanService) {
-        this.uzbekistanService = uzbekistanService;
-    }
-
-    @GetMapping("/uzbekistan")
-    public ResponseEntity getUzbekistanRegionByNumber(@RequestParam String region) {
+    @GetMapping("/region/{region}")
+    public ResponseEntity getUzbekistanRegionByNumber(final @PathVariable("region") String region) {
         try {
-            return ResponseEntity.ok(uzbekistanService.getOne(region));
-        } catch (RegionNotFoundException exception) {
-            return ResponseEntity.badRequest().body(exception.getMessage());
-        } catch (Exception exception) {
+            return ResponseEntity.ok(uzbekistanService.getUzbekistanPlatesByRegion(region));
+        } catch (RegionNotFoundException | RuntimeException exception) {
             return ResponseEntity.badRequest().body(exception.getMessage());
         }
     }
 
-    @GetMapping("/uzbekistan/all")
-    public ResponseEntity getAllUzbekistanRegions() {
+    @GetMapping("/description/{description}")
+    public ResponseEntity getUzbekistanRegionByDescription(final @PathVariable("description") String description) {
         try {
-            return ResponseEntity.ok(uzbekistanService.getAllRegions());
-        } catch (RegionsNotFoundException exception) {
-            return ResponseEntity.badRequest().body(exception.getMessage());
-        } catch (Exception exception) {
+            return ResponseEntity.ok(uzbekistanService.getUzbekistanPlatesByDescription(description));
+        } catch (DescriptionNotFoundException | RuntimeException exception) {
             return ResponseEntity.badRequest().body(exception.getMessage());
         }
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Iterable<UzbekistanEntity>> getAllUzbekistanRegions() {
+        return ResponseEntity.ok(uzbekistanService.getAllRegions());
     }
 }
--- src/main/java/com/regions/simpleregions/model/UzbekistanModel.java
+++ src/main/java/com/regions/simpleregions/model/UzbekistanModel.java
@@ -1,22 +1,29 @@
 package com.regions.simpleregions.model;
 
 import com.regions.simpleregions.entity.UzbekistanEntity;
+import lombok.Data;
 
+import java.util.Optional;
+
+@Data
 public class UzbekistanModel {
 
     private String description;
 
-    public String getDescription() {
-        return description;
+    private String region;
+
+    public static UzbekistanModel toModelByRegion(Optional<UzbekistanEntity> entity) {
+        UzbekistanModel model = new UzbekistanModel();
+        model.setDescription(entity.get().getDescription());
+        model.setRegion(entity.get().getRegion());
+
+        return model;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public static UzbekistanModel toModel(UzbekistanEntity entity) {
+    public static UzbekistanModel toModelByDescription(UzbekistanEntity entity) {
         UzbekistanModel model = new UzbekistanModel();
         model.setDescription(entity.getDescription());
+        model.setRegion(entity.getRegion());
 
         return model;
     }
