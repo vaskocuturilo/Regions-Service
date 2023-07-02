317a29a717475b4425096235a3e5ac0bbbce6703
asm <vaskocuturilo@rambler.ru>
2023-07-02 18:37:05 +0200

    DV-000167: Fix Slovakia country.

--- src/main/java/com/regions/simpleregions/model/SlovakiaModel.java
+++ src/main/java/com/regions/simpleregions/model/SlovakiaModel.java
@@ -1,22 +1,29 @@
 package com.regions.simpleregions.model;
 
 import com.regions.simpleregions.entity.SlovakiaEntity;
+import lombok.Data;
 
+import java.util.Optional;
+
+@Data
 public class SlovakiaModel {
 
     private String description;
 
-    public String getDescription() {
-        return description;
+    private String region;
+
+    public static SlovakiaModel toModelByRegion(Optional<SlovakiaEntity> entity) {
+        SlovakiaModel model = new SlovakiaModel();
+        model.setDescription(entity.get().getDescription());
+        model.setRegion(entity.get().getRegion());
+
+        return model;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public static SlovakiaModel toModel(SlovakiaEntity entity) {
+    public static SlovakiaModel toModelByDescription(SlovakiaEntity entity) {
         SlovakiaModel model = new SlovakiaModel();
         model.setDescription(entity.getDescription());
+        model.setRegion(entity.getRegion());
 
         return model;
     }
--- src/main/java/com/regions/simpleregions/service/SlovakiaService.java
+++ src/main/java/com/regions/simpleregions/service/SlovakiaService.java
@@ -1,28 +1,50 @@
 package com.regions.simpleregions.service;
 
 import com.regions.simpleregions.entity.SlovakiaEntity;
+import com.regions.simpleregions.exception.DescriptionNotFoundException;
 import com.regions.simpleregions.exception.RegionNotFoundException;
 import com.regions.simpleregions.model.SlovakiaModel;
 import com.regions.simpleregions.respository.SlovakiaRepo;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Data
 @Service
 public class SlovakiaService {
 
-    SlovakiaRepo slovakiaRepo;
+    private final SlovakiaRepo slovakiaRepo;
 
-    @Autowired
-    public SlovakiaService(SlovakiaRepo slovakiaRepo) {
-        this.slovakiaRepo = slovakiaRepo;
+    @Value("${notification.region.message}")
+    private String regionNotFound;
+
+    @Value("${notification.description.message}")
+    private String descriptionNotFound;
+
+    public SlovakiaModel getSlovakiaPlatesByRegion(final String region) throws RegionNotFoundException {
+        Optional<SlovakiaEntity> slovakiaRegion = slovakiaRepo.findByRegion(region);
+
+        slovakiaRegion
+                .stream()
+                .filter(slovakiaEntity -> slovakiaEntity.getRegion().equalsIgnoreCase(region))
+                .findFirst().orElseThrow(() -> new RegionNotFoundException(String.format(regionNotFound, region)));
+
+        return SlovakiaModel.toModelByRegion(slovakiaRegion);
     }
 
-    public SlovakiaModel getOne(String region) throws RegionNotFoundException {
-        SlovakiaEntity slovakiaRegion = slovakiaRepo.findByRegion(region);
-        if (slovakiaRegion == null) {
-            throw new RegionNotFoundException("Region not found.");
-        }
-        return SlovakiaModel.toModel(slovakiaRegion);
+    public List<SlovakiaModel> getSlovakiaPlatesByDescription(final String description) throws DescriptionNotFoundException {
+        List<SlovakiaEntity> slovakiaEntityList = slovakiaRepo.findByDescription(description);
+
+        slovakiaEntityList
+                .stream()
+                .map(slovakiaEntity -> slovakiaEntity.getDescription().equalsIgnoreCase(description))
+                .findAny().orElseThrow(() -> new DescriptionNotFoundException(String.format(descriptionNotFound, description)));
+
+        return slovakiaEntityList.stream().map(SlovakiaModel::toModelByDescription).collect(Collectors.toList());
     }
 
     public Iterable<SlovakiaEntity> getAllRegions() {
