b1ca2d3bbbbb684766ecfb74ab6284cca3864148
asm <vaskocuturilo@rambler.ru>
2023-07-12 21:15:22 +0200

    DV-000186: Add Allure report.

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,9 @@
 const { defineConfig } = require("cypress");
-const allureWriter = require('@shelex/cypress-allure-plugin/writer'); 
 
 module.exports = defineConfig({
 
   viewportHeight: 1080,
   viewportWidth: 1920,
-  "video": false,
   "retries": {
     "runMode": 2,
     "openMode": 0
@@ -13,7 +11,6 @@ module.exports = defineConfig({
 
   e2e: {
     setupNodeEvents(on, config) {
-      allureWriter(on, config);
       if (config.env.master) {
         return {
           baseUrl: "",
