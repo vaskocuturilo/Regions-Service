90ef176b890c9b398f5d1b6632b83783c2a169b3
asm <vaskocuturilo@rambler.ru>
2023-07-23 20:55:39 +0200

    DV-000201: Add integration backend with frontend.

--- cypress.config.js
+++ cypress.config.js
@@ -23,7 +23,7 @@ module.exports = defineConfig({
         };
       } else
         return {
-          baseUrl: "http://localhost:8080",
+          baseUrl: "http://localhost:3000",
           env: {
             env: "qa"
           },
