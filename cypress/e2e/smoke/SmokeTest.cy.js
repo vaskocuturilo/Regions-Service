95a3775c55e0094bea7e795750b32d49d2b03db4
asm <vaskocuturilo@rambler.ru>
2023-08-03 19:21:29 +0200

    DV-000210: Fix ui issues.

---
+++ cypress/e2e/smoke/SmokeTest.cy.js
@@ -0,0 +1,17 @@
+/// <reference types ="cypress"/>
+
+import {mainPage} from "../../support/pages/MainPage.cy"
+
+describe('Testing main page for application regions', () => {
+    beforeEach(() => {
+        cy.visit('/')
+    })
+
+    it('This is simple test for check main page', () => {
+        mainPage.checkDefaultMainPageElementsAppears();
+        mainPage.checkAppearsPrivatePlatesBlock(); 
+        mainPage.checkAppearsDiplomaticPlatesBlock();
+        mainPage.checkAppearsUploadPhotoBlock();
+        mainPage.checkAppearsCountriesInDropDown();
+    })
+}) 
