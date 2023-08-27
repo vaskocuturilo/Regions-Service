7f78baa79c12e8bff19a9cbd1b60bf83c0e1d281
asm <vaskocuturilo@rambler.ru>
2023-08-27 16:36:30 +0200

    DV-000236: Add filet by API KEY.

---
+++ src/main/java/com/regions/simpleregions/config/APIKeyAuthFilter.java
@@ -0,0 +1,24 @@
+package com.regions.simpleregions.config;
+
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class APIKeyAuthFilter  extends AbstractPreAuthenticatedProcessingFilter {
+
+    private final String principalRequestHeader;
+
+    public APIKeyAuthFilter(String principalRequestHeader) {
+        this.principalRequestHeader = principalRequestHeader;
+    }
+
+    @Override
+    protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
+        return request.getHeader(principalRequestHeader);
+    }
+
+    @Override
+    protected Object getPreAuthenticatedCredentials(HttpServletRequest request) {
+        return "N/A";
+    }
+}
---
+++ src/main/java/com/regions/simpleregions/config/APISecurityConfig.java
@@ -0,0 +1,34 @@
+package com.regions.simpleregions.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+public class APISecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${http.auth-token-header-name}")
+    private String principalRequestHeader;
+
+    @Value("${http.auth-token}")
+    private String principalRequestValue;
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        APIKeyAuthFilter filter = new APIKeyAuthFilter(principalRequestHeader);
+        filter.setAuthenticationManager(authentication -> {
+            String principal = (String) authentication.getPrincipal();
+            if (!principalRequestValue.equals(principal)) {
+                throw new BadCredentialsException(String.format("The API key = %s was not found or not the expected value.", principal));
+            }
+            authentication.setAuthenticated(true);
+            return authentication;
+        });
+        httpSecurity.
+                antMatcher("/api/v1/**").
+                csrf().disable().
+                sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
+                and().addFilter(filter).authorizeRequests().anyRequest().authenticated();
+    }
+}
