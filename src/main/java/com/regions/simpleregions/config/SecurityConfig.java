f6a328ca3b017381abf57f25902e9ec62c9c194d
asm <vaskocuturilo@rambler.ru>
2023-08-28 18:10:53 +0200

    DV-000236: Add Security Config.

---
+++ src/main/java/com/regions/simpleregions/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.regions.simpleregions.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final UserAuthenticationEntryPoint userAuthenticationEntryPoint;
+    private final UserAuthenticationProvider userAuthenticationProvider;
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .exceptionHandling(customizer -> customizer.authenticationEntryPoint(userAuthenticationEntryPoint))
+                .addFilterBefore(new JwtAuthFilter(userAuthenticationProvider), BasicAuthenticationFilter.class)
+                .csrf().disable()
+                .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers()
+                        .permitAll()
+                        .anyRequest().authenticated());
+        return http.build();
+    }
+}
