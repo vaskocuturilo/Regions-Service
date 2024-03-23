94d2fd57c672e70907055e8837aa6e3ef97bb5c1
asm <vaskocuturilo@rambler.ru>
2024-03-23 17:15:12 +0100

    DV-000294: Fix germany controller.

--- src/main/java/com/regions/simpleregions/respository/GermanyRepo.java
+++ src/main/java/com/regions/simpleregions/respository/GermanyRepo.java
@@ -6,12 +6,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface GermanyRepo extends CrudRepository<GermanyEntity, Long> {
-    Optional<GermanyEntity> findByRegion(String region);
+    List<GermanyEntity> findByRegion(String region);
 
-    @Query("select gernamy from GermanyEntity gernamy WHERE gernamy.description LIKE CONCAT('%',:description,'%')")
+    @Query("select gernamy from GermanyEntity gernamy WHERE gernamy.description LIKE CONCAT('%',:description,'%') order by gernamy.description desc ")
     List<GermanyEntity> findByDescription(String description);
 }
