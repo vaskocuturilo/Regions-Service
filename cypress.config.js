f9e6cb926e62d2e19315f6fd01b4eb4bdf1da1a8
asm <vaskocuturilo@rambler.ru>
2024-08-12 20:56:17 +0200

    DV-000297: Add Nginx config.

--- cypress.config.js
+++ cypress.config.js
@@ -1,32 +1,31 @@
-const { defineConfig } = require("cypress");
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+const {defineConfig} = require("cypress");
 
 module.exports = defineConfig({
 
-  viewportHeight: 1080,
-  viewportWidth: 1920,
-  "video": false,
-  "retries": {
-    "runMode": 2,
-    "openMode": 0
-  },
-
-  e2e: {
-    setupNodeEvents(on, config) {
-      if (config.env.master) {
-        return {
-          baseUrl: "http://regions-service.us-east-2.elasticbeanstalk.com/",
-          env: {
-            env: "master"
-          },
-        };
-      } else
-        return {
-          baseUrl: "http://localhost",
-          env: {
-            env: "qa"
-          },
-        };
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    "video": false,
+    "retries": {
+        "runMode": 2,
+        "openMode": 0
     },
-  },
+
+    e2e: {
+        setupNodeEvents(on, config) {
+            if (config.env.master) {
+                return {
+                    baseUrl: "http://regions-service.us-east-2.elasticbeanstalk.com/",
+                    env: {
+                        env: "master"
+                    },
+                };
+            } else
+                return {
+                    baseUrl: "http://localhost",
+                    env: {
+                        env: "qa"
+                    },
+                };
+        },
+    },
 });
